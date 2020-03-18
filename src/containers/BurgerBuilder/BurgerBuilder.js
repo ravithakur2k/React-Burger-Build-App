b3907552d2b9749b4b02f4265ee039158beedc6d
Ravi Thakur <43831111+ravithakur2k@users.noreply.github.com>
2020-03-17 23:49:31 -0400

    Post Request in Burger Builder
    Learnt about post request using Firebase as a back end api to post burger builder data

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,9 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -28,7 +31,8 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 4,
         purchaseable: false,
-        purchasing: false
+        purchasing: false,
+        loading: false
     }
 
     updatePurchaseState = ingredients => {
@@ -54,7 +58,29 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert("You Continue!!")
+        this.setState({
+            loading:true
+        })
+        const order = {
+            ingredients : this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Ravi Thakur',
+                address: {
+                    steet: "Test Rd",
+                    zipCode: "123456",
+                    country: "Germany",
+                },
+                email: "test@gmail.com"
+            },
+            deliveryMethod: 'fastest'
+        }
+        // alert("You Continue!!")
+        axios.post('/orders',order).then(Response => {
+            this.setState({loading:false, purchasing: false})
+        }).catch(error => {
+            this.setState({loading: false, purchasing: false})
+        })
     }
 
     addIngredientCHandler = (type) => {
@@ -96,11 +122,20 @@ class BurgerBuilder extends Component {
         for(let key in disabledInfo) {
             disabledInfo[key] = disabledInfo[key] <=0;
         }
+        let orderSummary = <OrderSummary 
+        totalPrice={this.state.totalPrice} 
+        ingredients = {this.state.ingredients} 
+        purchaseContinue={this.purchaseContinueHandler} 
+        purchaseCancelled={this.purchaseCancelOrder} />
+
+        if(this.state.loading){
+            orderSummary = <Spinner />
+        }
 
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelOrder}>
-                    <OrderSummary totalPrice={this.state.totalPrice} ingredients = {this.state.ingredients} purchaseContinue={this.purchaseContinueHandler} purchaseCancelled={this.purchaseCancelOrder} />
+                    {orderSummary}
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
@@ -115,4 +150,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
