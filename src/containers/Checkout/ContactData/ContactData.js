ab14d440b74b26f2a2900241fad0a1564bf4a0fc
RTHAKUR3 <rthakur3@ford.com>
2020-04-12 15:11:13 -0400

    Improvements and Fixes

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,6 +8,7 @@ import Spinner from "../../../components/UI/Spinner/Spinner";
 import Input from "../../../components/UI/Input/Input";
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
+import { updateObject, checkValidity } from '../../../shared/utility';
 
 
 class ContactData extends Component {
@@ -118,32 +119,17 @@ class ContactData extends Component {
     
   };
 
-  checkValidity(value, rules) {
-    let isValid = true;
-    if(rules.required){
-      isValid = value.trim() !== '' && isValid;
-    }
-    if(rules.minLength) {
-      isValid = value.trim().length >= rules.minLength && isValid
-    }
-    if(rules.maxLength) {
-      isValid = value.trim().length <= rules.maxLength && isValid
-    }
-
-    return isValid;
-
-  }
   inputChangedHandler = (event,inputIdentifier) => {
-    const updatedOrderForm = {
-      ...this.state.orderForm
-    }
-    const updatedFormElement = {
-      ...updatedOrderForm[inputIdentifier]
-    };
-    updatedFormElement.value = event.target.value;
-    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
-    updatedFormElement.touched = true;
-    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    
+    const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier],{
+      value:event.target.value,
+      valid:checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+      touched:true
+    });
+
+    const updatedOrderForm = updateObject(this.state.orderForm, {
+      [inputIdentifier] : updatedFormElement
+    })
 
     let formIsValid = true;
     for(let inputIdentifier in updatedOrderForm){
@@ -160,7 +146,6 @@ class ContactData extends Component {
         config: this.state.orderForm[key],
       });
     }
-    console.log(formElementsArray);
     let form = (
       <form>
         {formElementsArray.map((formElement) => (
